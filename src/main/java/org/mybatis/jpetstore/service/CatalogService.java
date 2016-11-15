fb0b03246b9b62b21981197d74bd146e895f5b83
Jie.Liu <ecliujie@gmail.com>
2016-11-15 22:54:16 +0800

    testing for thread block

--- src/main/java/org/mybatis/jpetstore/service/CatalogService.java
+++ src/main/java/org/mybatis/jpetstore/service/CatalogService.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2010-2015 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2010-2015 the original author or authors.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.mybatis.jpetstore.service;
 
@@ -34,46 +34,61 @@ import org.springframework.stereotype.Service;
 @Service
 public class CatalogService {
 
-  @Autowired
-  private CategoryMapper categoryMapper;
-  @Autowired
-  private ItemMapper itemMapper;
-  @Autowired
-  private ProductMapper productMapper;
+    @Autowired
+    private CategoryMapper categoryMapper;
+    @Autowired
+    private ItemMapper itemMapper;
+    @Autowired
+    private ProductMapper productMapper;
 
-  public List<Category> getCategoryList() {
-    return categoryMapper.getCategoryList();
-  }
-
-  public Category getCategory(String categoryId) {
-    return categoryMapper.getCategory(categoryId);
-  }
-
-  public Product getProduct(String productId) {
-    return productMapper.getProduct(productId);
-  }
-
-  public List<Product> getProductListByCategory(String categoryId) {
-    return productMapper.getProductListByCategory(categoryId);
-  }
-
-  public List<Product> searchProductList(String keywords) {
-    List<Product> products = new ArrayList<Product>();
-    for(String keyword : keywords.split("\\s+")){
-      products.addAll(productMapper.searchProductList("%" + keyword.toLowerCase() + "%"));
+    public List<Category> getCategoryList() {
+        return categoryMapper.getCategoryList();
     }
-    return products;
-  }
 
-  public List<Item> getItemListByProduct(String productId) {
-    return itemMapper.getItemListByProduct(productId);
-  }
+    public Category getCategory(String categoryId) {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return categoryMapper.getCategory(categoryId);
+    }
 
-  public Item getItem(String itemId) {
-    return itemMapper.getItem(itemId);
-  }
+    public Product getProduct(String productId) {
+        return productMapper.getProduct(productId);
+    }
 
-  public boolean isItemInStock(String itemId) {
-    return itemMapper.getInventoryQuantity(itemId) > 0;
-  }
+    public List<Product> getProductListByCategory(String categoryId) {
+        return productMapper.getProductListByCategory(categoryId);
+    }
+
+    public List<Product> searchProductList(String keywords) {
+        List<Product> products = new ArrayList<Product>();
+        for (String keyword : keywords.split("\\s+")) {
+            products.addAll(productMapper.searchProductList("%" + keyword.toLowerCase() + "%"));
+        }
+        return products;
+    }
+
+    public List<Item> getItemListByProduct(String productId) {
+        return itemMapper.getItemListByProduct(productId);
+    }
+
+    private Object lock = new Object();
+
+    public Item getItem(String itemId) {
+        synchronized (lock) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return itemMapper.getItem(itemId);
+    }
+
+    public boolean isItemInStock(String itemId) {
+        return itemMapper.getInventoryQuantity(itemId) > 0;
+    }
 }
